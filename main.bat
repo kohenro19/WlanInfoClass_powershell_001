74fbd6ab21cb5d77aa4fa60d31ab62fe356ffde5
kohenro19@gmail.com <kohenro19@gmail.com>
2025-02-01 10:14:28 +0900

    add the comments

--- main.bat
+++ main.bat
@@ -1,6 +1,13 @@
+REM バッチファイルの実行時にコマンドの出力を非表示にする
 @echo off
 
+REM PowerShellスクリプト verifyWlan.ps1 を実行
+REM -NoProfile: ユーザーのプロファイルを読み込まずに実行し、処理を高速化
+REM -ExecutionPolicy Unrestricted: 実行ポリシーを無制限に設定し、スクリプトの実行を許可
 powershell -NoProfile -ExecutionPolicy Unrestricted .\verifyWlan.ps1
 
+REM ユーザーがキーを押すまで一時停止（メッセージなし）
 pause > nul
+
+REM スクリプトを終了
 exit
